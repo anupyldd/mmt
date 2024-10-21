09bb6da721356fcd7fc5ef0a199f280be329e798
anupyldd <anupyldd@gmail.com>
2024-10-21 20:00:33 +0300

    init

---
+++ src/HillsAndDales.cpp
@@ -0,0 +1,12 @@
+﻿// HillsAndDales.cpp : Defines the entry point for the application.
+//
+
+#include "HillsAndDales.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
