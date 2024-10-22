d7917cbe8876ce4ca015cd889faeb4acfcc6a2fa
anupyldd <anupyldd@gmail.com>
2024-10-22 10:36:52 +0300

    fix linking for imgui

--- src/HillsAndDales.cpp
+++ src/HillsAndDales.cpp
@@ -2,10 +2,46 @@
 //
 
 #include "utility/Log.h"
+#include "rlImGui.h"
+#include "imgui.h"
 
 int main()
 {
 	using namespace hnd;
+
+	int screenWidth = 1280;
+	int screenHeight = 800;
+
+	SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
+	InitWindow(screenWidth, screenHeight, "raylib-Extras [ImGui] example - simple ImGui Demo");
+	SetTargetFPS(60);
+	rlImGuiSetup(true);
+
+	// Main game loop
+	while (!WindowShouldClose())    // Detect window close button or ESC key
+	{
+		BeginDrawing();
+		ClearBackground(DARKGRAY);
+
+		// start ImGui Conent
+		rlImGuiBegin();
+
+		// show ImGui Content
+		bool open = true;
+		ImGui::ShowDemoWindow(&open);
+
+		// end ImGui Content
+		rlImGuiEnd();
+
+		EndDrawing();
+		//----------------------------------------------------------------------------------
+	}
+	rlImGuiShutdown();
+
+	// De-Initialization
+	//--------------------------------------------------------------------------------------   
+	CloseWindow();        // Close window and OpenGL context
+	//--------------
 
 	return 0;
 }
