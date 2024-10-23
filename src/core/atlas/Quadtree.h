39e4ae639826d3bd3ba16b4f0daea479fe1b7487
anupyldd <anupyldd@gmail.com>
2024-10-23 22:04:45 +0300

    quadtree wip

---
+++ src/core/atlas/Quadtree.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "MapObject.h"
+
+#include "raylib.h"
+
+#include <vector>
+#include <memory>
+#include <array>
+
+namespace hnd
+{
+	namespace core
+	{
+		constexpr int QuadtreeNodeCapacity = 4;
+		using MapObjShrPtr = std::shared_ptr<MapObject>;
+		
+		class QuadtreeNode
+		{
+		public:
+			QuadtreeNode() = default;
+			QuadtreeNode(const Rectangle& bound);
+
+			bool InsertObject(MapObject obj);
+			std::vector<MapObjShrPtr> Query(const Rectangle& rect, std::vector<MapObjShrPtr> found) const;
+
+		private:
+			void Subdivide();
+
+		private:
+			Rectangle	boundary;
+			bool		divided = false;
+
+			std::array<MapObjShrPtr, QuadtreeNodeCapacity> objects;
+
+			std::unique_ptr<QuadtreeNode>	northWest,
+											northEast,
+											southWest,
+											southEast;
+		};
+	}
+}
