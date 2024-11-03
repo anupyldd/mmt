79481332761ad7bdb51b577e97815861b5b423c6
anupyldd <anupyldd@gmail.com>
2024-11-03 14:04:37 +0300

    ecs mgr

---
+++ src/core/ecs/ECS.cpp
@@ -0,0 +1,53 @@
+#include "ECS.h"
+
+namespace hnd
+{
+	namespace core
+	{
+		EntityManager::EntityManager()
+		{
+			for (Entity ent = 0; ent < MAX_ENTITIES; ++ent)
+			{
+				availableEntities.push(ent);
+			}
+		}
+		std::optional<Entity> EntityManager::CreateEntity()
+		{
+			if (livingEntityCount >= MAX_ENTITIES)
+			{
+				LOG_ERROR(std::format("Too many entities. Max: {}", MAX_ENTITIES));
+				return;
+			}
+
+			Entity id = availableEntities.front();
+			availableEntities.pop();
+			return id;
+		}
+		void EntityManager::DestroyEntity(Entity entity)
+		{
+			if (entity >= MAX_ENTITIES)
+				LOG_ERROR(std::format("Unable to remove entity {}, entity out of range", entity));
+
+			signatures[entity].reset();
+			availableEntities.push(entity);
+			--livingEntityCount;
+		}
+		void EntityManager::SetSignature(Entity entity, Signature signature)
+		{
+			if (entity >= MAX_ENTITIES)
+				LOG_ERROR(std::format("Unable to set signature for entity {}, entity out of range", entity));
+
+			signatures[entity] = signature;
+		}
+		Signature EntityManager::GetSignature(Entity entity)
+		{
+			if (livingEntityCount >= MAX_ENTITIES)
+			{
+				LOG_ERROR(std::format("Unable to access signature of entity {}, entity out of range", entity));
+				return emptySignature;
+			}
+
+			return signatures[entity];
+		}
+	}
+}
---
+++ src/core/ecs/ECS.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstdint>
+#include <bitset>
+#include <queue>
+#include <array>
+#include <optional>
+
+#include "../../utility/Log.h"
+
+namespace hnd
+{
+	namespace core
+	{
+		using Entity = uint32_t;
+		constexpr Entity MAX_ENTITIES = 5000;
+		
+		using ComponentType = uint8_t;
+		constexpr ComponentType MAX_COMPONENTS = 32;
+
+		using Signature = std::bitset<MAX_COMPONENTS>;
+		constexpr Signature emptySignature;
+
+		//------------------------
+
+		class EntityManager
+		{
+		public:
+			EntityManager();
+			
+			std::optional<Entity> CreateEntity();
+			void DestroyEntity(Entity entity);
+			void SetSignature(Entity entity, Signature signature);
+			Signature GetSignature(Entity entity);
+
+		private:
+			std::queue<Entity> availableEntities;
+			std::array<Signature, MAX_ENTITIES> signatures;
+			uint32_t livingEntityCount = 0;
+		};
+	}
+}
