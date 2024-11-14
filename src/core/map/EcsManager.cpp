e38f88dc842f60d04622845d84a8ec77757517f9
anupyldd <anupyldd@gmail.com>
2024-11-14 13:45:23 +0300

    ecs mgr

--- src/core/map/EcsManager.cpp
+++ src/core/map/EcsManager.cpp
@@ -4,56 +4,68 @@ namespace hnd
 {
 	namespace core
 	{
-		void EcsManager::CreateInstance(const std::string& mapName, int maxEntityCount)
+		void EcsManager::CreateInstance(const std::string& atlasName, const std::string& mapName, int maxEntityCount)
 		{
-			if (instances.contains(mapName))
+			if (InstanceExists(atlasName, mapName))
 			{
 				LOG_ERROR(std::format("Failed to create ECS instance for map {}: instance for this map already exists", mapName));
 				return;
 			}
 
-			instances[mapName] = std::make_unique<EcsInstance>();
-			instances.at(mapName)->Init(maxEntityCount);
+			instances[atlasName][mapName] = std::make_unique<EcsInstance>();
+			instances.at(atlasName).at(mapName)->Init(maxEntityCount);
 		}
 		void EcsManager::LoadInstace(const std::string& atlasName, const std::string& mapName)
 		{
-			if (instances.contains(mapName))
+			if (InstanceExists(atlasName, mapName))
 			{
 				LOG_ERROR(std::format("Failed to load ECS instance for map {}: instance for this map is already loaded", mapName));
 				return;
 			}
 
-			instances[mapName] = std::make_unique<EcsInstance>();
-			instances.at(mapName)->Load(GetInstanceFilePath(atlasName, mapName));
+			instances[atlasName][mapName] = std::make_unique<EcsInstance>();
+			instances.at(atlasName).at(mapName)->Load(GetInstanceFilePath(atlasName, mapName));
 		}
-		void EcsManager::DestroyInstance(const std::string& mapName)
+		void EcsManager::DestroyInstance(const std::string& atlasName, const std::string& mapName)
 		{
-			if (!instances.contains(mapName))
+			if (!InstanceExists(atlasName, mapName))
 			{
 				LOG_ERROR(std::format("Failed to destroy ECS instance for map {}: instance does not exist", mapName));
 				return;
 			}
 
-			instances.at(mapName)->Destroy();
-			instances.erase(mapName);
+			instances.at(atlasName).at(mapName)->Destroy();
+			instances.at(atlasName).erase(mapName);
 		}
 		std::filesystem::path EcsManager::GetInstanceFilePath(const std::string& atlasName, const std::string& mapName) const
 		{
 			return util::GetPathToMapSaveFile(atlasName, mapName);
 		}
-		void EcsManager::SetActiveInstace(const std::string& mapName)
+		EcsInstance* EcsManager::GetActiveInstance() const
 		{
-			if (!instances.contains(mapName))
+			return activeInstance.second.second;
+		}
+		bool EcsManager::InstanceExists(const std::string& atlasName, const std::string& mapName) const
+		{
+			return instances.contains(atlasName) && instances.at(atlasName).contains(mapName);
+		}
+		void EcsManager::SetActiveInstace(const std::string& atlasName, const std::string& mapName)
+		{
+			if (!InstanceExists(atlasName, mapName))
 			{
 				LOG_ERROR(std::format("Failed to set ECS instance for map {} as active: instance does not exist", mapName));
 				return;
 			}
 
-			activeInstance = instances.at(mapName).get();
+			activeInstance.second.second = instances.at(atlasName).at(mapName).get();
 		}
 		void EcsManager::UpdateActive()
 		{
-			if (activeInstance) activeInstance->Update();
+			if (GetActiveInstance()) GetActiveInstance()->Update();
+		}
+		ComponentId EcsManager::GetActiveComponentId(const std::string& compName) const
+		{
+			return GetActiveInstance()->ComponentGetId(compName);
 		}
 	}
 }
--- src/core/map/EcsManager.h
+++ src/core/map/EcsManager.h
@@ -2,6 +2,8 @@
 
 #include "EcsInstance.h"
 #include "MapUtil.h"
+#include "EcsTypes.h"
+#include "../../utility/Defines.h"
 
 #include <unordered_map>
 #include <vector>
@@ -13,32 +15,48 @@ namespace hnd
 {
 	namespace core
 	{
+
+		// { atlas_name : { map_name : ecs_instance } }
+		using EcsInstanceMap = std::unordered_map<std::string, std::unordered_map<std::string, std::unique_ptr<EcsInstance>>>;
+		// { atlas_name : { map_name : ecs_instance } }
+		using EcsActiveInstance = std::pair<std::string, std::pair<std::string, EcsInstance*>>;
+
 		/*
 		* manages ecs instances.
 		* each instance is associated with 1 map, 1 map can have only 1 instance.
-		* instances are loaded/saved/etc by map name
+		* instances are loaded/saved/etc by map name.
+		* ecs manager works in together with atlases, but not managed by them.
+		* only one instance of EcsManager exists in a program.
 		*/
 		class EcsManager
 		{
+			SINGLETON(EcsManager);
+
 		public:
 			// creates default instance
-			void CreateInstance(const std::string& mapName, int maxEntityCount);
+			void CreateInstance(const std::string& atlasName, const std::string& mapName, int maxEntityCount);
 
-			// loads previously saved instance from file by id
+			// loads previously saved instance from file by name
 			void LoadInstace(const std::string& atlasName, const std::string& mapName);
 
-			void DestroyInstance(const std::string& mapName);
+			void DestroyInstance(const std::string& atlasName, const std::string& mapName);
 
-			void SetActiveInstace(const std::string& mapName);
+			void SetActiveInstace(const std::string& atlasName, const std::string& mapName);
 
 			void UpdateActive();
 
+		public:
+
+			EcsInstance* GetActiveInstance() const;
+			ComponentId GetActiveComponentId(const std::string& compName) const;
+
 		private:
+			bool InstanceExists(const std::string& atlasName, const std::string& mapName) const;
 			std::filesystem::path GetInstanceFilePath(const std::string& atlasName, const std::string& mapName) const;
 
 		private:
-			std::unordered_map<std::string, std::unique_ptr<EcsInstance>> instances;
-			EcsInstance* activeInstance = nullptr;
+			EcsInstanceMap instances;
+			EcsActiveInstance activeInstance;
 		};
 	}
 }
