e6e57c7fae0f831034e65f50531f935ab20da443
anupyldd <anupyldd@gmail.com>
2025-01-23 11:40:24 +0300

    new res structs

--- src/core/map/Resources.h
+++ src/core/map/Resources.h
@@ -26,12 +26,55 @@ namespace mmt
 			std::shared_ptr<ResType> res;
 		};
 
-		// ------------------------------------------
-
-		struct PackRegistry
+		struct MmtTexture
 		{
-			std::map<std::string, bool> shouldLoad;
+			Texture2D tex = { 0 };
+
+			MmtTexture() = default;
+			MmtTexture(const MmtTexture& src) { tex = src.tex; }
+			MmtTexture(const std::string& path) 
+			{
+				Image img = LoadImage(path.c_str());
+				tex = LoadTextureFromImage(img);
+				UnloadImage(img);
+			}
+			~MmtTexture() { UnloadTexture(tex); }
 		};
+
+		struct MmtObject 
+		{
+			Texture2D obj = { 0 };
+
+			MmtObject() = default;
+			MmtObject(const MmtObject& src) { obj = src.obj; }
+			MmtObject(const std::string& path)
+			{
+				Image img = LoadImage(path.c_str());
+				obj = LoadTextureFromImage(img);
+				UnloadImage(img);
+			}
+			~MmtObject() { UnloadTexture(obj); }
+		};
+
+		struct MmtFont
+		{
+			Font fnt;
+
+			MmtFont() = default;
+			MmtFont(const MmtFont& src) { fnt = src.fnt; }
+			MmtFont(const std::string& path)
+			{
+				fnt = LoadFont(path.c_str());
+			}
+			~MmtFont() { UnloadFont(fnt); }
+		};
+
+		struct MmtScript
+		{
+			// TODO
+		};
+
+		// ------------------------------------------
 
 		bool IsSupportedImageFormat(const std::string& ext);
 
